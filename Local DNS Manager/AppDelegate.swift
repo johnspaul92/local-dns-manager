89e67802d9ec6cd4aa10f0b5391bb7e4c806743e
Johns Paul <johns001@e.ntu.edu.sg>
2018-02-28 15:51:24 +0800

    Initial Commit

---
+++ Local DNS Manager/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Local DNS Manager
+//
+//  Created by Johns Paul on 28/2/18.
+//  Copyright © 2018 TechNut. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
